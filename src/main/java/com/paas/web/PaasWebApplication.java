e6e848baa69ea0c861432228cbf3f8b352e5bd24
xiaozq <xiaozq@bbtree.com>
2018-08-09 20:44:30 +0800

    init

---
+++ src/main/java/com/paas/web/PaasWebApplication.java
@@ -0,0 +1,12 @@
+package com.paas.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaasWebApplication.class, args);
+	}
+}
