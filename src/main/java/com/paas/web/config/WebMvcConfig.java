987cce9a11f0576831f182e2bf88b24def234086
xiaozq <xiaozq@bbtree.com>
2018-12-24 09:36:40 +0800

    登录验证改成拦截器方式

--- src/main/java/com/paas/web/config/WebMvcConfig.java
+++ src/main/java/com/paas/web/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import com.paas.web.interceptors.CorsInterceptor;
 import com.paas.web.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
